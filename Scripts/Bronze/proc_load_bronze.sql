d0be7d2d8ac5e366972fe516a8256c03f3ba3715
Rania-Trabelsi <raniiatrabelsi@gmail.com>
2025-12-16 11:20:23 +0100

    Update file paths in proc_load_bronze.sql to use repository-relative paths

--- Scripts/Bronze/proc_load_bronze.sql
+++ Scripts/Bronze/proc_load_bronze.sql
@@ -38,7 +38,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_cust_info;
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\rania\Desktop\SQL\Data with Baraa\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM '/repo/Datasets/source_crm/cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -54,7 +54,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\rania\Desktop\SQL\Data with Baraa\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM '/repo/Datasets/source_crm/prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -69,7 +69,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_sales_details;
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\rania\Desktop\SQL\Data with Baraa\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM '/repo/Datasets/source_crm/sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -88,7 +88,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_loc_a101;
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\rania\Desktop\SQL\Data with Baraa\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		FROM '/repo/Datasets/source_erp/loc_a101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -103,7 +103,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_cust_az12;
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\rania\Desktop\SQL\Data with Baraa\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		FROM '/repo/Datasets/source_erp/cust_az12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -118,7 +118,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\rania\Desktop\SQL\Data with Baraa\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		FROM '/repo/Datasets/source_erp/px_cat_g1v2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
